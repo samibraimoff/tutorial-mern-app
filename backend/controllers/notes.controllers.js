1cf99868e2f70ce3c7235796af6e5b7e1f82f306
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-04-10 15:28:29 -0400

    error handling added

--- backend/controllers/notes.controllers.js
+++ backend/controllers/notes.controllers.js
@@ -2,28 +2,32 @@
 // @route   GET /api/notes
 // @access  Private
 const getNotes = (req, res) => {
-  res.status(200).json({ msg: 'get notes' });
+  res.status(200).json({ message: 'get notes' });
 };
 
 // @desc    createNote
 // @route   POST /api/notes
 // @access  Private
 const createNote = (req, res) => {
-  res.status(200).json({ msg: 'create note' });
+  if (!req.body.message) {
+    res.status(400);
+    throw new Error('Please add a message field');
+  }
+  res.status(200).json({ message: 'create note' });
 };
 
 // @desc    updateNote
 // @route   PUT /api/notes/:id
 // @access  Private
 const updateNote = (req, res) => {
-  res.status(200).json({ msg: `update note ${req.params.id}` });
+  res.status(200).json({ message: `update note ${req.params.id}` });
 };
 
 // @desc    deleteNote
 // @route   DELETE /api/notes/:id
 // @access  Private
 const deleteNote = (req, res) => {
-  res.status(200).json({ msg: `delete note ${req.params.id}` });
+  res.status(200).json({ message: `delete note ${req.params.id}` });
 };
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/error.middleware');
 const PORT = process.env.PORT || 3030;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/notes', require('./routes/notes.routes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is started on port ${PORT}...`);
