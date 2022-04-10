4adc75f685ef11470c246e32f2a6d0090a227fca
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-04-10 16:10:49 -0400

    model and CRUD functions added in controllers

--- backend/controllers/notes.controllers.js
+++ backend/controllers/notes.controllers.js
@@ -1,35 +1,57 @@
 const asyncHandler = require('express-async-handler');
+const Note = require('../models/Note.model');
 
 // @desc    getNotes
 // @route   GET /api/notes
 // @access  Private
 const getNotes = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get notes' });
+  const notes = await Note.find();
+  res.status(200).json(notes);
 });
 
 // @desc    createNote
 // @route   POST /api/notes
 // @access  Private
 const createNote = asyncHandler(async (req, res) => {
-  if (!req.body.message) {
+  if (!req.body.text) {
     res.status(400);
     throw new Error('Please add a message field');
   }
-  res.status(200).json({ message: 'create note' });
+
+  const note = await Note.create({
+    text: req.body.text,
+  });
+  res.status(200).json(note);
 });
 
 // @desc    updateNote
 // @route   PUT /api/notes/:id
 // @access  Private
 const updateNote = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `update note ${req.params.id}` });
+  const note = await Note.findById(req.params.id);
+  if (!note) {
+    res.status(400);
+    throw new Error('Note not found');
+  }
+
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedNote);
 });
 
 // @desc    deleteNote
 // @route   DELETE /api/notes/:id
 // @access  Private
 const deleteNote = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `delete note ${req.params.id}` });
+  const note = await Note.findById(req.params.id);
+  if (!note) {
+    res.status(400);
+    throw new Error('Note not found');
+  }
+  await note.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
