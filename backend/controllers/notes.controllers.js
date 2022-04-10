58f3b013c8ab109742a3d5e776a740d81e20cfcd
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-04-10 15:49:16 -0400

    mongodb connected

--- backend/controllers/notes.controllers.js
+++ backend/controllers/notes.controllers.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    getNotes
 // @route   GET /api/notes
 // @access  Private
-const getNotes = (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'get notes' });
-};
+});
 
 // @desc    createNote
 // @route   POST /api/notes
 // @access  Private
-const createNote = (req, res) => {
+const createNote = asyncHandler(async (req, res) => {
   if (!req.body.message) {
     res.status(400);
     throw new Error('Please add a message field');
   }
   res.status(200).json({ message: 'create note' });
-};
+});
 
 // @desc    updateNote
 // @route   PUT /api/notes/:id
 // @access  Private
-const updateNote = (req, res) => {
+const updateNote = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update note ${req.params.id}` });
-};
+});
 
 // @desc    deleteNote
 // @route   DELETE /api/notes/:id
 // @access  Private
-const deleteNote = (req, res) => {
+const deleteNote = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete note ${req.params.id}` });
-};
+});
 
 module.exports = {
   getNotes,
