22a1223bd38440b121a621887452caae387fc605
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-04-11 02:06:18 -0400

    pages and react router dom added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <div>
-      <h1>app</h1>
-    </div>
-  );
-}
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './pages/Dashboard';
+import { Login } from './pages/Login';
+import { Register } from './pages/Register';
 
-export default App;
+export const App = () => {
+  return (
+    <Fragment>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
+};
