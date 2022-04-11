e3f2a1b4bfc63e149602d55fded9d5c0ddb094df
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-04-11 02:14:03 -0400

    header and navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './pages/Dashboard';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <Fragment>
       <Router>
         <div className='container'>
+          <Header />
           <Routes>
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
